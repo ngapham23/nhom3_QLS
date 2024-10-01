43b17a10a48917a30618dabb22ebdb0842eca626
Phạm Quang Ngà <okamibada@gmail.com>
2024-10-01 10:10:40 +0700

    cập nhập một số giao diện(Ngà Phạm)

---
+++ frm_NXB.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLySach
+{
+    public partial class frm_NXB : Form
+    {
+        public frm_NXB()
+        {
+            InitializeComponent();
+        }
+        LopChung lopchung = new LopChung();
+
+        public void LoadNXB()
+        {
+            string sql = "Select * from NhaXuatBan";
+            dt_GridNXB.DataSource = lopchung.LoadDL(sql);
+        }
+        private void btn_Them_Click(object sender, EventArgs e)
+        {
+            string sql = "Insert into NhaXuatBan values ('" + txt_MaNXB.Text + "',N'" + txt_TenNXB.Text + "',N'" + txt_DiaChi.Text + "',N'" + txt_SoDienThoai.Text + "')";
+            int kq = lopchung.themsuaxoa(sql);
+            if (kq >= 1) MessageBox.Show("Thêm Nhà xuất bản thành công");
+            else MessageBox.Show("Thêm Nhà xuất bản thất bại");
+            LoadNXB();
+        }
+
+        private void btn_Xoa_Click(object sender, EventArgs e)
+        {
+            string sql = "Delete NhaXuatBan where MaNXB = '" + txt_MaNXB.Text + "'";
+            int kq = lopchung.themsuaxoa(sql);
+            if (kq >= 1) MessageBox.Show("Xóa Nhà xuất bản thành công");
+            else MessageBox.Show("Xóa Nhà xuất bản thất bại");
+            LoadNXB();
+        }
+
+        private void btn_Sua_Click(object sender, EventArgs e)
+        {
+            string sql = "Update NhaXuatBan set TenNXB = N'" + txt_TenNXB.Text + "', DiaChi = N'" + txt_DiaChi.Text + "', DienThoai = N'" + txt_SoDienThoai.Text + "' where MaNXB = '" + txt_MaNXB.Text + "'";
+            int kq = lopchung.themsuaxoa(sql);
+            if (kq >= 1) MessageBox.Show("Sửa Nhà xuất bản thành công");
+            else MessageBox.Show("Sửa Nhà xuất bản thất bại");
+            LoadNXB();
+        }
+
+        private void btn_HienThi_Click(object sender, EventArgs e)
+        {
+            LoadNXB();
+        }
+
+        private void frm_NXB_Load(object sender, EventArgs e)
+        {
+            LoadNXB();
+        }
+
+        public void load_TimKiem()
+        {
+            string sql = "Select * from NhaXuatBan where TenNXB like '%" + txt_TimKiem.Text + "%'";
+            DataTable dt = lopchung.LoadDL(sql);
+            dt_GridNXB.DataSource = dt;
+
+        }
+
+        private void txt_TimKiem_TextChanged(object sender, EventArgs e)
+        {
+            load_TimKiem();
+        }
+
+        private void dt_GridNXB_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txt_MaNXB.Text = dt_GridNXB.CurrentRow.Cells["MaNXB"].Value.ToString();
+            txt_TenNXB.Text = dt_GridNXB.CurrentRow.Cells["TenNXB"].Value.ToString();
+            txt_DiaChi.Text = dt_GridNXB.CurrentRow.Cells["DiaChi"].Value.ToString();
+            txt_SoDienThoai.Text = dt_GridNXB.CurrentRow.Cells["DienThoai"].Value.ToString();
+        }
+    }
+}
---
+++ frm_TacGia.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLySach
+{
+    public partial class frm_TacGia : Form
+    {
+        public frm_TacGia()
+        {
+            InitializeComponent();
+        }
+
+        LopChung lopchung = new LopChung();
+        private void frm_TacGia_Load(object sender, EventArgs e)
+        {
+            LoatTG();
+        }
+
+        private void btn_HienThi_Click(object sender, EventArgs e)
+        {
+            LoatTG();
+        }
+
+        private void btn_them_Click(object sender, EventArgs e)
+        {
+            string sql = "Insert into TacGia values ('" + txt_mtg.Text + "',N'" + txt_tentg.Text + "',N'" + txt_lienlac.Text + "')";
+            int kq = lopchung.themsuaxoa(sql);
+            if (kq >= 1) MessageBox.Show("Thêm tác giả thành công");
+            else MessageBox.Show("Thêm tác giả thất bại");
+            LoatTG();
+        }
+
+        private void btn_sua_Click(object sender, EventArgs e)
+        {
+            string sql = "UPDATE TacGia SET TenTacGia = N'" + txt_tentg.Text + "', QuocGia = N'" + txt_lienlac.Text + "' WHERE MaTacGia = '" + txt_mtg.Text + "'";
+            int kq = lopchung.themsuaxoa(sql);
+            if (kq >= 1) MessageBox.Show("Sửa tác giả thành công");
+            else MessageBox.Show("Sửa tác giả thất bại");
+            LoatTG();
+        }
+
+        private void btn_xoa_Click(object sender, EventArgs e)
+        {
+            string sql = "DELETE FROM TacGia WHERE MaTacGia = '" + txt_mtg.Text + "'";
+            int kq = lopchung.themsuaxoa(sql);
+            if (kq >= 1) MessageBox.Show("Xóa tác giả thành công");
+            else MessageBox.Show("Xóa tác giả thất bại");
+            LoatTG();
+        }
+
+        public void LoatTG()
+        {
+            string sql = "Select * from TacGia";
+            DataTable dt = lopchung.LoadDL(sql);
+            dt_gridTG.DataSource = dt;
+        }
+
+        private void dt_gridTG_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txt_mtg.Text = dt_gridTG.CurrentRow.Cells["MaTacGia"].Value.ToString();
+            txt_tentg.Text = dt_gridTG.CurrentRow.Cells["TenTacGia"].Value.ToString();
+            txt_lienlac.Text = dt_gridTG.CurrentRow.Cells["QuocGia"].Value.ToString();
+        }
+
+        public void load_TimKiem()
+        {
+            string sql = "Select * from TacGia where TenTacGia like '%" + txt_TimKiem.Text + "%'";
+            DataTable dt = lopchung.LoadDL(sql);
+            dt_gridTG.DataSource = dt;
+
+        }
+
+        private void txt_TimKiem_TextChanged(object sender, EventArgs e)
+        {
+            load_TimKiem();
+
+        }
+    }
+}
